f5ea79072f69d27371c2a7903dd3bf5f782e901c
Lillyqwe <lillysikveland@gmail.com>
2025-01-08 09:58:57 +0100

    Updates
    Small updates

---
+++ script.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    document.getElementById("myDIV").style.animation = "newmove 1s infinite";
+}
+function my2Function() {
+    document.getElementById("myDIV").style.animation = "mymove 1s infinite";
+}
